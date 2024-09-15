443a80077b620793892c3ddd35996b93bdee647b
Felipe Sodré <felipe.b4rros@gmail.com>
2024-09-15 00:53:15 -0300

    update

--- 01_spatio_temporal_data_analysis.py
+++ 01_spatio_temporal_data_analysis.py
@@ -1,5 +1,6 @@
 import folium
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import geopandas as gpd
 import pointpats as pp
@@ -7,30 +8,21 @@ import contextily as cx
 
 
 # Reading shooting GeoDataFrame
-gdf_shootings = gpd.read_parquet(
-    "./data/gdf_shootings.parquet"
-)
-# reading cities GeoDataFrame
-gdf_rm_salvador = gpd.read_file(
-    "./data/dados_BA.gpkg",
-    layer="gdf_rm_salvador")
-gdf_rm_salvador.to_crs(31984, inplace=True)
-
-# cria coluna datetime convertendo dado de data e hora como tal
-# gdf_shootings["datetime"] = pd.to_datetime(gdf_shootings["datetime"])
-# cria coluna de data e hora separando tais informacoes
-# gdf_tiroteios["date"] = gdf_tiroteios.datetime.dt.date
-# gdf_tiroteios["time"] = gdf_tiroteios.datetime.dt.time
-
-# Table with the number of shootings per city
-gdf_shootings.groupby(gdf_shootings.city_name).size().sort_values(
-    ascending=False
-).to_frame('shootings')
-
+gdf_shootings = gpd.read_parquet("./data/gdf_shootings.parquet")
 # Projecting CRS to UTM 24S
 gdf_shootings.to_crs(31984, inplace=True)
-# gdf_shootings_salvador = gdf_shootings[gdf_shootings["city_name"] == "SALVADOR"].copy()
 
+# reading cities GeoDataFrame
+gdf_rm_salvador = gpd.read_file("./data/dados_BA.gpkg", layer="gdf_rm_salvador")
+gdf_rm_salvador.to_crs(31984, inplace=True)
+
+# Reproduce table 1:
+# Cities in the metropolitan region of Salvador with their total shootings recorded from July 1, 2022, to June 30, 2024..
+gdf_shootings.groupby(gdf_shootings.city_name).size().sort_values(
+    ascending=False
+).to_frame("shootings")
+
+# running global Knox test for each city
 results = []
 for city in gdf_shootings["city_name"].unique():
     gdf_tiroteios_city = gdf_shootings[gdf_shootings["city_name"] == city].copy()
@@ -46,41 +38,33 @@ for city in gdf_shootings["city_name"].unique():
     )
 
     results.append(
-    {
-        "city": city,
-        "n_tiros": gdf_tiroteios_city.shape[0],
-        "p_poisson": tiros_knox.p_poisson,
-        "p_sim": tiros_knox.p_sim,
-        "mean_tau": gdf_tiroteios_city["time_in_days"].mean(),
-    }
+        {
+            "city": city,
+            "n_tiros": gdf_tiroteios_city.shape[0],
+            "p_poisson": tiros_knox.p_poisson,
+            "p_sim": tiros_knox.p_sim,
+            "mean_tau": gdf_tiroteios_city["time_in_days"].mean(),
+        }
     )
-
 
 results = pd.DataFrame(results)
 
-# Organizing data to test spatio-temporal clustering
 # filtering cities with p-value lower or equals to 0.05
+# Reproduce table 2:
+# Cities in the metropolitan region of Salvador with a deviation from the null hypothesis,
+# indicating a systematic spatiotemporal correlation in the Knox Global Test.
 results.loc[(results.p_poisson <= 0.05) | (results.p_sim <= 0.05)].reset_index(
     drop=True
-)
+)[["city", "n_tiros", "p_sim"]]
 
+# Preparing data to test spatio-temporal [local] clustering
 # Creating date sequence
 min_date = gdf_shootings.date.min()
 
 # Caculating days passed since the first shooting in 2023
 gdf_shootings["time_in_days"] = gdf_shootings.date.apply(lambda x: x - min_date).dt.days
 
-# Global Knox Spatio-temporal test
-tiros_knox = pp.Knox.from_dataframe(
-    gdf_shootings,
-    time_col="time_in_days",
-    delta=300,
-    tau=5
-)
-print("p_poisson: ", tiros_knox.p_poisson)
-print("p_sim: ", tiros_knox.p_sim)
-
-# Local Knox Spatio-temporal test
+# Local Knox Local Spatio-temporal test
 tiros_knox_local = pp.KnoxLocal.from_dataframe(
     gdf_shootings,
     time_col="time_in_days",
@@ -90,30 +74,22 @@ tiros_knox_local = pp.KnoxLocal.from_dataframe(
     keep=False,
 )
 
-
-tiros_knox_local.hotspots()
-
-tiros_knox_local._gdfhs()
-
 tiros_knox_local_ = tiros_knox_local.hotspots()
-print(tiros_knox_local_.shape)
+# joining result with cities GeoDataFrame to identify the city of each shooting
 shootings_spatemp_hotspot = tiros_knox_local_.sjoin(gdf_rm_salvador)
 
+# saving shootings_spatemp_hotspot as GeoPackage
 shootings_spatemp_hotspot.to_file(
-    "./data/dados_BA.gpkg",
-    layer="shootings_spatemp_hotspot"
+    "./data/dados_BA.gpkg", layer="shootings_spatemp_hotspot"
 )
 
 # filtering spatio-temporal hotspots with p-value lower or equals to 0.05
-shootings_spatemp_hotspot = shootings_spatemp_hotspot.loc[shootings_spatemp_hotspot.pvalue <= 0.05]
-print(tiros_knox_local_.shape)
-shootings_spatemp_hotspot_orientation = (
-    shootings_spatemp_hotspot.groupby(shootings_spatemp_hotspot.orientation).size().to_frame()
-)
+shootings_spatemp_hotspot = shootings_spatemp_hotspot.loc[
+    shootings_spatemp_hotspot.pvalue <= 0.05
+]
 
-shootings_spatemp_hotspot_orientation
-
-# table with the number of shootings per city and orientation
+# Reproduce table 3:
+# Knox local statistics showing the number of lead, lag, and coincident shooting observations for each city.
 shootings_spatemp_hotspot.groupby(
     [shootings_spatemp_hotspot.name_muni, shootings_spatemp_hotspot.orientation]
 ).size().to_frame("value").sort_values(by="value", ascending=False)
@@ -124,18 +100,68 @@ tiros_knox_local.plot(
     ax=ax, colors={"focal": "red", "neighbor": "yellow", "nonsig": "none"}
 )
 cx.add_basemap(
-    ax, crs=gdf_rm_salvador.to_crs(31984).crs,
-    #source=cx.providers.HERE.satelliteDay
+    ax,
+    crs=gdf_rm_salvador.to_crs(31984).crs,
+    # source=cx.providers.HERE.satelliteDay
 )
 # Obter os limites do GeoDataFrame tiros_knox_local
-xmin, ymin, xmax, ymax = shootings_spatemp_hotspot.total_bounds
+xmin, ymin, xmax, ymax = gdf_rm_salvador.total_bounds
 
 # Ajustar os limites do gráfico para os limites de salva
 ax.set_xlim(xmin, xmax)
 ax.set_ylim(ymin, ymax)
+matplotlib.use('TkAgg')
+plt.show()
+
+
+
+
+# Criar a figura e os eixos para os subplots
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(15, 7))
+
+# Mapa de Referência
+gdf_rm_salvador.plot(ax=ax1, facecolor="none", edgecolor="black")
+tiros_knox_local.plot(ax=ax1, colors={"focal": "red", "neighbor": "yellow", "nonsig": "none"})
+cx.add_basemap(ax1, crs=gdf_rm_salvador.to_crs(31984).crs)
+ax1.set_title('Mapa de Referência')
+
+# Definir limites para o zoom (substitua com suas coordenadas)
+# xmin_zoom, ymin_zoom, xmax_zoom, ymax_zoom = (-38.5, -12.5, -37.5, -11.5)
+xmin_zoom, ymin_zoom, xmax_zoom, ymax_zoom = (-38.51886, -12.99604, -38.50775, -11.00704)
+
+# Mapa com Zoom
+gdf_rm_salvador.plot(ax=ax2, facecolor="none", edgecolor="black")
+tiros_knox_local.plot(ax=ax2, colors={"focal": "red", "neighbor": "yellow", "nonsig": "none"})
+cx.add_basemap(ax2, crs=gdf_rm_salvador.to_crs(31984).crs)
+ax2.set_xlim(xmin_zoom, xmax_zoom)
+ax2.set_ylim(ymin_zoom, ymax_zoom)
+ax2.set_title('Mapa com Zoom')
+
+# Ajustar layout
+plt.tight_layout()
+
+# Mostrar o gráfico
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 e = tiros_knox_local.explore()
 folium.GeoJson(gdf_rm_salvador).add_to(e)
 e.add_child(folium.map.LayerControl())
-e
+e.save("Knox_local_test_result.html")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Applying spatio-temporal analysis for data mining on shooting data
 
-Repository with data and procedures to reproduce the analysis done in the paper "Applying spatio-temporal analysis for data mining on shooting data" accepted in the [Belém FOSS4G 2024](https://2024.foss4g.org) event.
+Repository with data and procedures to reproduce the analysis done for the paper "Applying spatio-temporal analysis for data mining on shooting data" accepted to be presented in the [FOSS4G 2024 Belém](https://2024.foss4g.org) event.
 
 # Setting up the environment
 Create a virtual environment and install the dependencies with the following commands:
@@ -11,14 +11,15 @@ pip install --upgrade pip
 pip install -r requirements.txt
 ```
 
-## .env
-Create a `.env` file in the root of the repository with `email` and `password` access for [Fogo Cruzado API](https://api.fogocruzado.org.br/sign-up):
+## Setup .env file
+Create a `.env` file in the root of the repository with `EMAIL` and `PASSWORD` variables with respective information registered in the [Fogo Cruzado API](https://api.fogocruzado.org.br/sign-up):
 ```
 EMAIL=user@host.com
 PASSWORD=password
 ```
 
+In case you don't have an account, you can create one [here](https://api.fogocruzado.org.br/sign-up).
+Also, this repository already has the data collected in the `data` folder, for reproducibility.
+
 # Analysis
-The analysis is divided into two parts: the first part is the data collection and the second part is the data analysis.
-[00_getting_data.py](./00_getting_data.py)
-For reproducibility, the data is already available in the `data` repository's folder.
+The analysis is divided into two parts: the first part is the data collection, which can be found in the [00_getting_data.py](./00_getting_data.py) file, and the second part is the data analysis, which can be found in the [01_data_analysis.py](./01_data_analysis.py) file.
